0f987095c116dc3bdd6ad705b0dbe294adba8b05
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-29 20:59:21 +0600

    forOf loop done

---
+++ forOf.js
@@ -0,0 +1,9 @@
+//array er modde all items one by one print korar jonno amra for of loop user kori.
+
+const numbers = [12, 33, 44, 56, 21, 45, 98, 75];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log("for off number: " + num);
+}
